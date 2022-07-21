8df9fd1ae9d9700a33ed4d65b98cd551427d7d9a
Hyoung-Kyu Song <hksong35@mindslab.ai>
2022-07-21 11:48:42 +0900

    Merge branch 'master'

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,67 +1,6 @@
 import React from 'react';
-import clsx from 'clsx';
 import styles from './styles.module.css';
 
-const FeatureList = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
-
-function Feature({ Svg, title, description }) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
-
-// export default function HomepageFeatures() {
-//   return (
-//     <section className={styles.features}>
-//       <div className="container">
-//         <div className="row">
-//           {FeatureList.map((props, idx) => (
-//             <Feature key={idx} {...props} />
-//           ))}
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
 
 export default function HomepageFeatures() {
   return (
@@ -70,28 +9,18 @@ export default function HomepageFeatures() {
         <div className={styles.pageContents}>
 
           <br />
-
-          <h1 class="anchor anchorWithStickyNavbar_mojV" id="deepkyu-blog">
-            블로그를 시작합니다.
-            <a class="hash-link" href="#블로그를-시작합니다"
-              title="Direct link to heading"></a>
-          </h1>
-          <br />
-
-          <p>
-            딥러닝과 관련해서 제가 읽는 재밌는 논문들을 많이 소개하고 싶습니다.
+          <p dir="auto">
+            <a href="https://github.com/deepkyu">
+              <img src="https://camo.githubusercontent.com/f6d50128cb007f85916b7a899da5d94f654dce35a37331c8d28573aef46f4274/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6769746875622d2532333132313031312e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d676974687562266c6f676f436f6c6f723d7768697465" alt="GitHub" data-canonical-src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&amp;logo=github&amp;logoColor=white"></img></a> <a href="https://www.youtube.com/channel/UCZF23gx3j-8KvahXcSdzZaA" rel="nofollow">
+              <img src="https://camo.githubusercontent.com/165da3cf9ab80bd8395e6de8c28077d68982319c6ecf871f6f54443c68886840/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f596f75547562652d2532334646303030302e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d596f7554756265266c6f676f436f6c6f723d7768697465" alt="YouTube" data-canonical-src="https://img.shields.io/badge/YouTube-%23FF0000.svg?style=for-the-badge&amp;logo=YouTube&amp;logoColor=white"></img></a> <a href="mailto:song@deepkyu.me">
+              <img src="https://camo.githubusercontent.com/571384769c09e0c66b45e39b5be70f68f552db3e2b2311bc2064f0d4a9f5983b/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f476d61696c2d4431343833363f7374796c653d666f722d7468652d6261646765266c6f676f3d676d61696c266c6f676f436f6c6f723d7768697465" alt="Gmail" data-canonical-src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&amp;logo=gmail&amp;logoColor=white"></img></a> <a href="https://www.linkedin.com/in/deepkyu/" rel="nofollow">
+              <img src="https://camo.githubusercontent.com/7e1a1a039c75a7c4d2a91d7f97bf0a1c2adcf7cb49b7dbbfc02963a4f9fdaca4/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6c696e6b6564696e2d2532333030373742352e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d6c696e6b6564696e266c6f676f436f6c6f723d7768697465" alt="LinkedIn" data-canonical-src="https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&amp;logo=linkedin&amp;logoColor=white"></img></a> <a href="https://www.instagram.com/deep.kyu/" rel="nofollow">
+              <img src="https://camo.githubusercontent.com/7a705494c370a8412797521701153d2873fb39109edf80afc408efd0927ae2d0/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f496e7374616772616d2d2532334534343035462e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d496e7374616772616d266c6f676f436f6c6f723d7768697465" alt="Instagram" data-canonical-src="https://img.shields.io/badge/Instagram-%23E4405F.svg?style=for-the-badge&amp;logo=Instagram&amp;logoColor=white"></img></a> <a href="https://twitter.com/deepkyu_song" rel="nofollow">
+              <img src="https://camo.githubusercontent.com/42312e937813abe3eee7d5e9bb08df31605b20af03dd433c4a0dd6233f9fad16/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f547769747465722d2532333144413146322e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d54776974746572266c6f676f436f6c6f723d7768697465" alt="Twitter" data-canonical-src="https://img.shields.io/badge/Twitter-%231DA1F2.svg?style=for-the-badge&amp;logo=Twitter&amp;logoColor=white"></img></a> <a href="https://namecard.kakao.com/deepkyu" rel="nofollow">
+              <img src="https://camo.githubusercontent.com/616b5ebfb2b5e0ce0fe6b79f7623e7ba52eca53e53fa5a00bc08dbb5d643ebc3/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6b616b616f74616c6b2d6666636430302e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d6b616b616f74616c6b266c6f676f436f6c6f723d303030303030" alt="KakaoTalk" data-canonical-src="https://img.shields.io/badge/kakaotalk-ffcd00.svg?style=for-the-badge&amp;logo=kakaotalk&amp;logoColor=000000"></img></a>
           </p>
-
-          <p>
-            더불어서 제가 재밌게 하고 있는 연구들도 소개해드리고 싶고요.
-          </p>
-
-          <p>
-            마지막으로 저라는 사람에 대해 소개해드리고자 합니다.
-          </p>
-
-          <p>
-            재밌는 공간이 되도록 잘 꾸며보겠습니다 😁
+          <p dir="auto">
+            And my <a href="https://huggingface.co/spaces/CVPR/ml-talking-face" rel="nofollow">Hugging Face Spaces<g-emoji class="g-emoji" alias="hugs" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/1f917.png">🤗</g-emoji></a>
           </p>
 
 
